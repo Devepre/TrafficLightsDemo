6b5def2fce7b340938ce1749b16e712b7edbfc53
Devepre <bicycle2life@gmail.com>
2018-02-07 12:39:02 +0200

    refactoring creating light view object

--- TrafficLightsDemo/ViewController.m
+++ TrafficLightsDemo/ViewController.m
@@ -1,4 +1,5 @@
 #import "ViewController.h"
+#import "DELControllerWorldUI.h"
 
 @interface ViewController ()
 
@@ -6,25 +7,31 @@
 @property (weak, nonatomic) UIImageView *imageViewTwo;
 @property (weak, nonatomic) UIImageView *imageViewThree;
 
+@property (strong, nonatomic) NSMutableArray *images;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _images = [[NSMutableArray alloc] init];
+    
+    [self createLightWithXcoord:170 andYcoord:100 andWidth:50 andColors:[[NSArray alloc] initWithObjects:[UIColor redColor], [UIColor yellowColor], [UIColor greenColor], nil]];
 
-    UIImageView *imageViewOne = [self createLightViewWithColor:[UIColor redColor] andCoordX:170 andCoordY:100 andWidth:100 andHeight:100];
-    self.imageViewOne = imageViewOne;
-    
-    UIImageView *imageViewTwo = [self createLightViewWithColor:[UIColor yellowColor] andCoordX:170 andCoordY:200 andWidth:100 andHeight:100];
-    self.imageViewTwo = imageViewTwo;
-    
-    UIImageView *imageViewThree = [self createLightViewWithColor:[UIColor greenColor] andCoordX:170 andCoordY:300 andWidth:100 andHeight:100];
-    self.imageViewThree = imageViewThree;
-    
-    [self.view addSubview:imageViewOne];
-    [self.view addSubview:imageViewTwo];
-    [self.view addSubview:imageViewThree];
+}
+
+- (void)createLightWithXcoord:(CGFloat)coordX andYcoord:(CGFloat)coordY andWidth:(CGFloat)width andColors:(NSArray<UIColor *> *)colors {
+    CGFloat currentX = coordX;
+    CGFloat currentY = coordY;
+    for (UIColor *currentColor in colors) {
+        UIImageView *imageViewCurrent = [self createLightViewWithColor:currentColor andCoordX:currentX andCoordY:currentY andWidth:width andHeight:width];
+        [self.images addObject:imageViewCurrent];
+        [self.view addSubview:imageViewCurrent];
+        
+//        currentX+= width;
+        currentY+= width;
+    }
 }
 
 - (UIImageView *)createLightViewWithColor:(UIColor *)color andCoordX:(CGFloat)coordX andCoordY:(CGFloat)coordY andWidth:(CGFloat)width andHeight:(CGFloat)height  {
@@ -37,7 +44,12 @@
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
-    [self blinkView:self.imageViewTwo withDuration:0.5f];
+//    [self blinkView:self.imageViewTwo withDuration:0.5f];
+    [self blinkView:[self.images objectAtIndex:1] withDuration:0.5f];
+    
+    
+    DELControllerWorldUI *world = [[DELControllerWorldUI alloc] init];
+//    [world start];
 }
 
 - (void) blinkView:(UIView *)view withDuration:(double)duration {
