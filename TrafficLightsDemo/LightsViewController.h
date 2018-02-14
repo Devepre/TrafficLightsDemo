d4b143912bfb523c4e05ca314f73dd1df1470071
Devepre <bicycle2life@gmail.com>
2018-02-14 14:43:20 +0200

    adding buttons

--- TrafficLightsDemo/LightsViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "DELControllerWorldUIDelegate.h"
-
-@interface LightsViewController : UIViewController<DELControllerWorldUIDelegate>
-
-@property (strong, nonatomic) IBOutlet UIView *contentView;
-
-@end
--- TrafficLightsDemo/Models/DELLightUI.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import "DELLight.h"
-
-@interface DELLightUI : NSObject
-
-@property (strong, nonatomic) NSMutableArray<UIImageView *> *lightStatesImages;
-
-@end
